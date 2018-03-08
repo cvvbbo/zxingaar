513aea765b63033c8e5ff376154b92ea4fc1837e
ONEBEAR <xiongzheng@emiaoqian.com>
2018-03-08 13:47:01 +0800

    first

---
+++ src/main/java/com/google/zxing/client/android/aaa.java
@@ -0,0 +1,19 @@
+package com.google.zxing.client.android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xiong on 2018/2/1.
+ */
+
+public class aaa extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aaa);
+    }
+}
